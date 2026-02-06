a7a84a71b10a92f037709279e1e654387fac035b
EyMlab <eymlab2131@gmail.com>
2026-02-06 16:01:15 -0500

    OCULTAR COMPRAS

--- js/nav.js
+++ js/nav.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       ? `<a href="inventario.html" class="${pagina === 'Inventario' ? 'active' : ''}">Inventario</a>`
       : "";
 
-    const enlaceCompras = 'admin'
+    const enlaceCompras = rol === 'admin'
       ? `<a href="compras.html" class="${pagina === 'Compras' ? 'active' : ''}">Compras</a>`
       : "";
 
