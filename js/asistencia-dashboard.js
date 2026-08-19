8fcb0bb23d2e51df3babdc2f6c4b6f35c40066a8
EyMlab <eymlab2131@gmail.com>
2026-08-19 13:43:31 -0500

    DASHBOARD EN ASISTENCIA

--- js/asistencia-dashboard.js
+++ js/asistencia-dashboard.js
@@ -63,6 +63,7 @@ async function cargarDashboard() {
   renderChartTendencia(data);
   renderChartLocalidad(data);
   renderChartSalidas(data);
+  renderTablaAusentes(data);
 }
 
 function renderKPIs(d) {
